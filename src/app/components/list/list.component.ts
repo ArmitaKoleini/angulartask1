af0d336792d6fe46e1d256ee15c9701581fbe833
ArmitaKoleini <armitakoleini02@gmail.com>
2024-02-26 14:44:43 +0330

    list box changes

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,60 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [NgFor, FormsModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  public users: string[] = ['a', 'b', 'c', 'd', 'e', 'f'];
+  public movedUsers: string[] = [];
+  public selectedUsers: string[] = [];
+  // public isEditing: boolean = false;
+
+  constructor() {}
+
+  // public moveRight() {
+  //   if (this.selectedUsers) {
+  //     this.users = this.users.filter((user) => user !== this.selectedUsers);
+  //     this.movedUsers.push(this.selectedUsers);
+  //     this.selectedUsers = null;
+  //   }
+  // }
+
+  public moveRight() {
+    if (this.selectedUsers.length > 0) {
+      this.users = this.users.filter(
+        (user) => !this.selectedUsers.includes(user)
+      );
+      this.movedUsers = this.movedUsers.concat(this.selectedUsers);
+      this.selectedUsers = [];
+    }
+  }
+
+  public moveLeft() {
+    if (this.selectedUsers.length > 0) {
+      this.movedUsers = this.movedUsers.filter(
+        (user) => !this.selectedUsers.includes(user)
+      );
+      this.users = this.users.concat(this.selectedUsers);
+      this.selectedUsers = [];
+    }
+  }
+
+  public removeFromList() {
+    if (this.selectedUsers) {
+      this.users = this.users.filter(
+        (user) => !this.selectedUsers.includes(user)
+      );
+    }
+    if (this.selectedUsers) {
+      this.movedUsers = this.movedUsers.filter(
+        (user) => !this.selectedUsers.includes(user)
+      );
+    }
+  }
+}
